3f3c5a4bb972114cd8b3c0236ae606ad41717dee
Sturion <sturion.douglas@gmail.com>
2021-04-07 22:19:14 -0300

    First Commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+function firstValue(firstValue){
+    x=firstValue;
+ }
+function secondValue(secondValue){
+    y=secondValue;
+ }
+ function thirdValue(thirdValue){
+     z=thirdValue;
+ }
+ function tolerance(tolerance){
+     tol = tolerance;
+ }
+ function mostraResult(){
+    res = x+y;
+    resFin = res*z;
+    if(resFin >= 1000 && resFin < 1000000){
+        resFin = resFin/1000;
+        tolMax = ((resFin/100)*tol)+resFin;
+    tolMin = resFin-((resFin/100)*tol);
+        document.getElementById("resultado").innerHTML = ("Resistor é "+ resFin+ "kΩ com tolerancia de entre " +tolMin+ "kΩ e "+tolMax+"kΩ");
+    }
+    else if (resFin >= 1000000){
+        resFin = resFin/1000000; 
+        tolMax = ((resFin/100)*tol)+resFin;
+    tolMin = resFin-((resFin/100)*tol);
+        document.getElementById("resultado").innerHTML = ("Resistor é "+ resFin+ "MΩ com tolerancia de entre " +tolMin+ "MΩ e "+tolMax+"kΩ");
+    }
+    else{
+        resFin = resFin;
+        tolMax = ((resFin/100)*tol)+resFin;
+    tolMin = resFin-((resFin/100)*tol);
+        document.getElementById("resultado").innerHTML = ("Resistor é "+ resFin+ "Ω com tolerancia de entre " +tolMin+ "Ω e "+tolMax+"Ω");
+    }
+ }
